9b4739ed48df48a76de4a1d619c02e45f85be14b
AroNicolas <hei.nicolas.5@gmail.com>
2025-06-11 16:39:23 +0300

    feat: implements an asynchronous email sending

---
+++ src/main/java/org/school/hei/service/event/SendEmailRequestedService.java
@@ -0,0 +1,24 @@
+package org.org.school.service.event;
+
+import org.org.school.endpoint.event.model.SendEmailRequested;
+import org.org.school.mail.Email;
+import org.org.school.mail.Mailer;
+import jakarta.mail.internet.InternetAddress;
+import java.util.List;
+import java.util.function.Consumer;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class SendEmailRequestedService implements Consumer<SendEmailRequested> {
+    private final Mailer mailer;
+
+    @SneakyThrows
+    @Override
+    public void accept(SendEmailRequested sendEmailRequested) {
+        InternetAddress recipientAddress = new InternetAddress(sendEmailRequested.getTo());
+        mailer.accept(new Email(recipientAddress, List.of(), List.of(), "", "... world!", List.of()));
+    }
+}
